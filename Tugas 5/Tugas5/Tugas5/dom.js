85d120524475093a21603753b32bb19dff2c7a21
Muhamad Iqbal Wijaksana <115132221+iqbalsmdn2@users.noreply.github.com>
2022-11-09 22:54:39 +0700

    Update dom.js

--- Tugas 5/Tugas5/Tugas5/dom.js
+++ Tugas 5/Tugas5/Tugas5/dom.js
@@ -1,26 +1,26 @@
-function hapusGambar(id) {
-  const hapus = document.getElementById(id);
-  hapus.remove();
-  const count = document.getElementById("basket").children.length - 1;
-  document.getElementById("count").innerHTML = count;
-}
+function hapus(id) {
+    const hapus = document.getElementById(id);
+    hapus.remove();
+    const count = document.getElementById("basket").children.length - 1;
+    document.getElementById("count").innerHTML = count;
+  }
+  
+  function deleteAndAdd(id, src) {
+    const count = document.getElementById("basket").children.length - 0;
+    const image = document.createElement("img");
+    image.setAttribute("id", count);
+    image.setAttribute("onclick", "hapus(id)");
+    image.src = `${src}`;
+    document.getElementById("basket").appendChild(image);
+    document.getElementById("count").innerHTML = count;
+  }
 
-function deleteAndAdd(id, src) {
-  const image = document.createElement("img");
-  image.setAttribute("id", "img1");
-  image.setAttribute("onclick", "hapusGambar(id)");
-  image.src = `${src}`;
-  document.getElementById("basket").appendChild(image);
-  const count = document.getElementById("basket").children.length - 1;
-  document.getElementById("count").innerHTML = count;
-}
-
-function start() {
+function backgroundColor() {
  var inputColor = prompt( "Enter a color name for the" + "Background-color","" );
  document.body.setAttribute("style","Background-color: " + inputColor );
 }
 
-function start2() 
+function textColor() 
 { 
     var inputColor = prompt( "Enter a color name for the" + "Background-color","" );
     document.getElementById("text1").setAttribute("style","color: "+ inputColor);
